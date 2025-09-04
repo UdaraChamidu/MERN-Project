1768e627c24d98bd511fb014d97056e14b7cb1af
udara <chamiduudara321@gmail.com>
2025-09-04 23:22:48 +0530

    users table addea

--- src/Users.js
+++ src/Users.js
@@ -1,9 +1,22 @@
 import UserForm from "./UserForm";
+import UserTable from "./UserTable";
+
+/* data handling part is doing in this page */
+/* these users should be sent to userTable using props */
+const users = [
+    {id:1, name:"Udara"},
+    {id:2, name:"John"},
+    {id:3, name:"Jane"}
+];
 
 const Users = () => {
     return(
-        <UserForm />
+        <>                  {/* React fragment, only one root should have. so <></> is used */}
+            <UserForm />
+            <UserTable rows={users} />    {/* users data is sent to UserTable using props, (rows is just a name). now i can access 'rows' in UserTable file */}
+        </>
     )
 }
 
 export default Users;
+
