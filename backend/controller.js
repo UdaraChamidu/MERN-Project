277f0b1b23ed6d47a32ac60cc503986a855056b2
udara <chamiduudara321@gmail.com>
2025-09-07 09:35:20 +0530

    Basic CRUD functions created

--- backend/controller.js
+++ backend/controller.js
@@ -1,23 +1,78 @@
-const e = require("express");
+/* const e = require("express");
 
 const users = [
     { id: 1, name: 'Udara' },
     { id: 2, name: 'John' },
     { id: 3, name: 'Jane' }
 ];
-
+ */
+/* here data is imported. instead of doing above...*/
+const users = require("./model"); /* import the dataset from model.js file*/
 
 /* controller function to return the data*/
 
-const getUsers = (cb) => {      /* cb = call back function*/
-    cb(users);          /* returnig with the dataset*/
+const getUsers = (req, res, next) => {
+  /* next  for do the next thing*/
+  User.find() /* select user from the database*/
+    .then((response) => {
+      res.json({ response }) /* return the response as json format*/;
+    })
+    .catch((error) => {
+      res.json({
+        error: error,
+      });
+    });
+};
+/* get data using a promis*/
+
+const addUser = (req, res, next) => {
+  const user = new User({
+    id: req.body.id,
+    name: req.body.name,
+  });
+  user
+    .save()
+    .then((response) => {
+      res.json({ response }) /* return the response as json format*/;
+    })
+    .catch((error) => {
+      res.json({
+        error: error,
+      });
+    });
 };
 
-const getUserById = (id, cb) => {
-    const user = users.find(user => user.id == id);  /* find user by id*/
-    cb(user);           /* returnig with the dataset*/
+const updataUser = (req, res, next) => {
+  const id = req.body.id;
+  const name = req.body.name;
+  /* const {id, name} = req.body -> using object re structuring*/
+  user
+    .updateOne({ id: id }, { $set: { name: name } })
+    .then((response) => {
+      res.json({ response }) /* return the response as json format*/;
+    })
+    .catch((error) => {
+      res.json({
+        error: error,
+      });
+    });
 };
 
-exports.getUsers = getUsers;   /* export the function to access from other files*/
-exports.getUserById = getUserById;
+const deleteUser = (req, res, next) => {
+  const id = req.body.id;
+  User.deleteOne({ id: id })
+    .then((response) => {
+      res.json({ response }) /* return the response as json format*/;
+    })
+    .catch((error) => {
+      res.json({
+        error: error,
+      });
+    });
+};
 
+exports.getUsers = getUsers; /* export the function to access from other files*/
+exports.addUser = addUser;
+exports.updataUser = updataUser;
+exports.deleteUser = deleteUser;
+/* now this is a controller */
