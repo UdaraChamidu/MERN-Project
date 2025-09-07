2885ee33630b43a3b4eb5ab7966ca0d906fc255f
udara <chamiduudara321@gmail.com>
2025-09-07 09:50:54 +0530

    creates routes for all 4 operations

--- router.js
+++ router.js
@@ -1,1 +1,11 @@
- 
+const express = require("express");
+const router = express.Router();
+const controller = require("./controller"); /* import controller.js file */
+
+/* create rest api routes for 4 methods*/
+router.get("/users",controller.getUsers); /* get all users (whatshould return: all users)*/
+router.post("/createuser", controller.addUser);
+router.post("/updateuser", controller.updataUser);
+router.post("/deleteuser", controller.deleteUser);
+
+module.exports = router;
