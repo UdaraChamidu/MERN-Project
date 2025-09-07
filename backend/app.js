e183c75da5c391e550b9aacc676107fbab778401
udara <chamiduudara321@gmail.com>
2025-09-07 09:45:36 +0530

    created CRUD end points.

--- backend/app.js
+++ backend/app.js
@@ -22,16 +22,26 @@ app.use(express.json());   /* to accept json data , convert data to json format*
 
 /* create rest api routes part (using express)*/
 app.get('/users', (req, res) => {   /*url, request and response */
-    controller.getUsers((users) => {   /* call the controller function */
-        res.send(users);   /* send the response to client */
+    controller.getUsers((req, res, next) => {   /* call the controller function */
+        res.send();   /* send the response to client */
     });
 });
-/* this will show all the users: http://localhost:5000/users */
 
-app.get('/user', (req, res) => {   /*url, request and response */
-    const id = req.query.id;   /* send the id from query parameter and get the user */
-    controller.getUserById(id, (user) => {   /* call the controller function */
-        res.send(user);   /* send the response to client */
+app.post('/createuser', (req, res) => {   /*url, request and response */
+    controller.addUser(req.body, (callback) => {   /* call the controller function */
+        res.send();   /* send the response to client */
+    });
+});
+
+app.post('/updateuser', (req, res) => {   /*url, request and response */
+    controller.updateUser(req.body, (callback) => {   /* call the controller function */
+        res.send(callback);   /* paa call back because i need to know which data was updated */
+    });
+});
+
+app.post('/deleteuser', (req, res) => {   /*url, request and response */
+    controller.deleteUser(req.body, (callback) => {   /* call the controller function */
+        res.send(callback);   /* paa call back because i need to know which data was updated */
     });
 });
 /* this will show user by id: http://localhost:5000/user?id=2 */
